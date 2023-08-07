81343c72d840c7638f0e01d26a215ddb14c81f47
LaughingCoffinLawl <electro600@live.it>
2023-08-07 03:03:15 +0200

    excersises with node

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...elements) {
+    const newArray = [];
+    array.forEach ((item) => {
+        if (!elements.includes(item)) {
+            newArray.push(item);
+        }
+    });
+    return newArray;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,30 @@
-const convertToCelsius = function() {
+
+/* (100 °F - 32) × 5/9 = 37,778 °C */
+
+const convertToCelsius = function(far) {
+let cel;
+
+  if (far < 0) {
+    cel = far - 32 * (5/9);
+  } else {
+    cel = far - 32 * (5/9);
+  }
+  
+  return cel;
 };
 
-const convertToFahrenheit = function() {
+
+/* (100 °C × 9/5) + 32 = 212 °F */
+
+const convertToFahrenheit = function(cel) {
+let far;
+
+  if (cel < 0) {
+    far = (cel * 5/9) + 32;
+  } else {
+    far = (cel * 5/9) + 32;
+  }
+  return far;
 };
 
 // Do not edit below this line
